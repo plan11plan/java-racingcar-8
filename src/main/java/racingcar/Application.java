29aee028d9984c79806024d417bfb1ea4010d25c
plan11plan <tjtfy7028@gmail.com>
2025-10-25 22:31:22 +0900

    feat(Application): 레이싱카 애플리케이션 기능 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,27 @@
 package racingcar;
 
+import java.util.List;
+import racingcar.model.CarName;
+import racingcar.model.Game;
+import racingcar.model.RandomNumberGenerator;
+import racingcar.model.Referee;
+import racingcar.model.RoundCount;
+import racingcar.model.WoowaCourseRandomNumberGenerator;
+import racingcar.view.ConsoleInputReader;
+import racingcar.view.GameInputView;
+import racingcar.view.GameOutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        GameInputView gameInputView = new GameInputView(new ConsoleInputReader());
+        GameOutputView gameOutputView = new GameOutputView();
+        List<CarName> carNames = gameInputView.readCarNames();
+        RoundCount roundCount = gameInputView.readRoundCount();
+        RandomNumberGenerator woowaCourseRandomNumberGenerator = new WoowaCourseRandomNumberGenerator();
+        Referee referee = new Referee();
+        Game game = Game.init(carNames, roundCount.roundCount(), referee);
+        game.play(woowaCourseRandomNumberGenerator, gameOutputView);
+
     }
 }
