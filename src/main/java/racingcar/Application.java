7823c0dfb6dac17fb950f4c324982960578af888
plan11plan <tjtfy7028@gmail.com>
2025-10-26 02:47:59 +0900

    fix(Application): Console 리소스 정리 로직 추가
    - 예외 발생 시 리소스 해제 하기 위함

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
 import racingcar.controller.RacingCarApplication;
 import racingcar.model.Referee;
 import racingcar.model.WoowaCourseRandomNumberGenerator;
@@ -10,14 +11,17 @@ import racingcar.view.GameOutputView;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        RacingCarApplication application = new RacingCarApplication(
-                new GameInputView(new ConsoleInputReader()),
-                new GameOutputView(),
-                new WoowaCourseRandomNumberGenerator(),
-                new Referee()
-        );
+        try {
+            RacingCarApplication application = new RacingCarApplication(
+                    new GameInputView(new ConsoleInputReader()),
+                    new GameOutputView(),
+                    new WoowaCourseRandomNumberGenerator(),
+                    new Referee()
+            );
 
-        application.run();
-
+            application.run();
+        } finally {
+            Console.close();
+        }
     }
 }
